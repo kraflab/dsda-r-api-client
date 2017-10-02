ac740dca35e1fe795c44319d390257dc6eabcfd2
Ryan Krafnick <kraflab@gmail.com>
2017-10-02 23:31:04 +0200

    Remove pointless comments

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,10 +1,8 @@
 class Hash
-  # Returns a hash that includes everything but the given keys.
   def except(*keys)
     dup.except!(*keys)
   end
 
-  # Replaces the hash without the given keys.
   def except!(*keys)
     keys.each { |key| delete(key) }
     self
