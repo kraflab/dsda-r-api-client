d7434a82122f14ccf1ca91e4c18d505999a31438
Ryan Krafnick <kraflab@gmail.com>
2017-10-08 23:19:21 +0200

    Add incident tracker

--- dsda-client.rb
+++ dsda-client.rb
@@ -5,6 +5,7 @@ require 'dsda_client/options'
 require 'dsda_client/api'
 require 'dsda_client/command_parser'
 require 'dsda_client/terminal'
+require 'dsda_client/incident_tracker'
 
 options        = DsdaClient::Options.new(ARGV)
 DsdaClient::Api.setup(options)
@@ -12,3 +13,4 @@ root_uri       = DsdaClient::Api.location
 command_parser = DsdaClient::CommandParser.new(root_uri, options)
 
 DsdaClient::Terminal.run(command_parser, ARGF.read)
+DsdaClient::IncidentTracker.dump
---
+++ lib/dsda_client/incident_tracker.rb
@@ -0,0 +1,18 @@
+require 'dsda_client/terminal'
+
+module DsdaClient
+  class IncidentTracker
+    class << self
+      def track(event_type, model, obj)
+        @incidents ||= {}
+        @incidents[event_type] ||= {}
+        @incidents[event_type][model] ||= []
+        @incidents[event_type][model] << obj
+      end
+
+      def dump
+        DsdaClient::Terminal.log_error(@incidents)
+      end
+    end
+  end
+end
