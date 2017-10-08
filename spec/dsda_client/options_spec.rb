0f953229d506dcd4cf0e7da75d843b2186ec26b4
Ryan Krafnick <kraflab@gmail.com>
2017-10-08 21:47:33 +0200

    Refactor options spec

--- spec/dsda_client/options_spec.rb
+++ spec/dsda_client/options_spec.rb
@@ -2,30 +2,34 @@ require 'spec_helper'
 require 'dsda_client/options'
 
 RSpec.describe DsdaClient::Options do
-  describe 'dump_requests?' do
+  describe 'defaults' do
+    subject(:options) { described_class.new([]) }
+
+    it { expect(options.dump_requests?).to be false }
+    it { expect(options.production?).to be true }
+    it { expect(options.post?).to be false }
+  end
+
+  describe '#dump_requests?' do
     context '--dump-requests option used' do
       it 'returns true' do
         expect(described_class.new(['--dump-requests']).dump_requests?).to be true
       end
     end
-
-    context 'no option used' do
-      it 'returns false' do
-        expect(described_class.new([]).dump_requests?).to be false
-      end
-    end
   end
 
-  describe 'production?' do
+  describe '#production?' do
     context '--local option used' do
       it 'returns false' do
         expect(described_class.new(['--local']).production?).to be false
       end
     end
+  end
 
-    context 'no option used' do
+  describe '#post?' do
+    context '--post options used' do
       it 'returns true' do
-        expect(described_class.new([]).production?).to be true
+        expect(described_class.new(['--post']).post?).to be true
       end
     end
   end
