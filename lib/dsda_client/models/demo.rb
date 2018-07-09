e0998c21b35283e9bbacee00a6baba4233b765fc
Ryan Krafnick <kraflab@gmail.com>
2018-07-09 21:07:32 +0200

    Update demo model

--- lib/dsda_client/models/demo.rb
+++ lib/dsda_client/models/demo.rb
@@ -3,8 +3,8 @@ require 'dsda_client/models/base'
 module DsdaClient
   module Models
     class Demo < DsdaClient::Models::Base
-      require_keys 'tas', 'guys', 'version', 'wad_username', 'file', 'engine',
-                   'time', 'level', 'levelstat', 'category_name', 'recorded_at',
+      require_keys 'tas', 'guys', 'version', 'wad', 'file', 'engine',
+                   'time', 'level', 'levelstat', 'category', 'recorded_at',
                    'players'
       allow_keys   'tags', 'compatibility', 'video_link'
     end
