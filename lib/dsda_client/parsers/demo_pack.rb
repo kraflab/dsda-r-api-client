e5155e6bf7b89f937af210d1ec38ff8c1e8d8504
Ryan Krafnick <kraflab@gmail.com>
2019-04-21 17:36:57 +0200

    Add unique demo pack parser

--- lib/dsda_client/parsers/demo_pack.rb
+++ lib/dsda_client/parsers/demo_pack.rb
@@ -8,6 +8,39 @@ module DsdaClient
       def model
         'demo_pack'
       end
+
+      def uri
+        URI(root_uri + '/demos')
+      end
+
+      def make_request
+        instance['demos'].each do |demo|
+          merge_file_data(demo)
+          post(demo)
+        end
+      end
+
+      def post(demo)
+        response = make_individual_request(demo)
+        if response['save']
+          @file_id = response['demo']['file_id']
+        end
+      end
+
+      def make_individual_request(demo)
+        RequestService.new(options).request(uri, headers, 'demo' => demo)
+      end
+
+      def merge_file_data(demo)
+        demo.delete('file')
+        demo.delete('file_id')
+
+        if @file_id
+          demo['file_id'] = @file_id
+        else
+          demo['file'] = instance['file']
+        end
+      end
     end
   end
 end
---
+++ spec/dsda_client/parsers/demo_pack_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require 'dsda_client/options'
+require 'dsda_client/command_parser'
+require 'dsda_client/parsers/demo_pack'
+
+describe DsdaClient::Parsers::DemoPack do
+  let(:options) do
+    instance_double(
+      DsdaClient::Options,
+      dump_requests?: false
+    )
+  end
+  let(:headers) { {} }
+  let(:root_uri) { 'foo' }
+  let(:instance) do
+    {
+      'file' => {
+        'name' => 'foo.zip',
+        'data' => 'FoO'
+      },
+      'demos' => [
+        { },
+        { }
+      ]
+    }
+  end
+  let(:parser) do
+    described_class.new(instance, root_uri, headers, options)
+  end
+  let(:response) do
+    {
+      'save' => true,
+      'demo' => {
+        'file_id' => 123
+      }
+    }
+  end
+  let(:requester) { instance_double(DsdaClient::RequestService) }
+
+  subject { described_class.call(instance, root_uri, headers, options) }
+
+  before do
+    allow(described_class).to receive(:new).and_return(parser)
+    allow(parser).to receive(:parse_file_data).and_return(true)
+    allow(DsdaClient::RequestService).to receive(:new).and_return(requester)
+    allow(requester).to receive(:request).and_return(response)
+  end
+
+  it 'uses the file id to only upload the file data once' do
+    expect(requester).to receive(:request) do |_, _, payload|
+      expect(payload['demo']['file']).not_to eq(nil)
+      expect(payload['demo']['file_id']).to eq(nil)
+    end.ordered.and_return(response)
+
+    expect(requester).to receive(:request) do |_, _, payload|
+      expect(payload['demo']['file']).to eq(nil)
+      expect(payload['demo']['file_id']).not_to eq(nil)
+    end.ordered.and_return(response)
+
+    subject
+  end
+end
